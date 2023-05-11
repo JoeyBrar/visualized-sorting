b56232bd6adf4a5147949f6999c06b97ecbffd10
JoeyBrar <jorabrar2007@gmail.com>
2023-05-11 15:38:58 -0400

    .

---
+++ src/MergeSort.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class MergeSort extends Sorting {
+    public MergeSort(ArrayList<Integer> toSortList) {
+        super(toSortList);
+    }
+
+    public int[] sort(int[] arr) {
+        return arr;
+    }
+
+}
---
+++ src/Sorting.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class Sorting {
+    private ArrayList<Integer> toSortList = new ArrayList<Integer>();
+
+    public Sorting(ArrayList<Integer> toSortList) {
+        this.toSortList = toSortList;
+    }
+
+    public boolean sortOnce(int i) {
+        return false;
+    }
+
+    public double calculateDisplaySpeed() {
+        return 0.0;
+    }
+
+    public void display(ArrayList<Integer> arr){
+        //display first case
+
+
+        for(int i=0;i<arr.size();i++) {
+            while(!sortOnce(i)) {
+                //display
+            }
+        }
+    }
+
+}
+
